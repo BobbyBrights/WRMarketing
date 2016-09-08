a6f97dd4765733e6a5e083188f7853dd934eb851
Allan Estrada <allan.prodigious@digitalbi.com>
2016-09-08 17:03:58 -0600

    Estructura del proyecto

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	//Slick slider
+	$('.masthead-slider').slick({
+		dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+	  autoplay: true,
+  	autoplaySpeed: 6000,
+	});
+
+
+
+});
