067c8736e3b3f9c0ce1140108e22c3ae18cdaa6d
Allan Estrada <allan.prodigious@digitalbi.com>
2016-09-09 11:37:39 -0600

    contact and icons

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	//Slick slider
 	$('.masthead-slider').slick({
 		dots: true,
-	  infinite: true,
-	  speed: 500,
-	  fade: true,
-	  cssEase: 'linear',
-	  autoplay: true,
-  	autoplaySpeed: 6000,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		autoplay: true,
+		autoplaySpeed: 6000
 	});
 
 
